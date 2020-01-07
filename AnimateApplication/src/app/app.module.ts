b95289418cc8571493637e1beb97de971ddc5dd1
azaac <arielsantos.ac@gmail.com>
2020-01-07 23:45:19 +0000

    +change square to green with animation

--- AnimateApplication/src/app/app.module.ts
+++ AnimateApplication/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -12,7 +12,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AnimateApplication/src/app/dashboard/dashboard.component.ts
+++ AnimateApplication/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { trigger, state, style } from '@angular/animations';
 
 @Component({
   selector: 'app-dashboard',
+  animations: [
+    trigger('signal', [
+      state('go', style ({
+        'background-color': 'green'
+      }))
+    ])
+  ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
