eb36351f2dec8061264f30dc89a8051cec1c321e
azaac <arielsantos.ac@gmail.com>
2020-01-07 23:53:08 +0000

    +add transition in signal change state

--- AnimateApplication/src/app/dashboard/dashboard.component.ts
+++ AnimateApplication/src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit  } from '@angular/core';
-import { trigger, state, style } from '@angular/animations';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-dashboard',
   animations: [
     trigger('signal', [
       state('go', style ({
-        'background-color': 'green'
+        'background-color': 'green',
+        'width': '100px',
+        'height': '100px'
       })),
       state('stop', style ({
-        'background-color': 'red'
-      }))
+        'background-color': 'red',
+        'width': '50px',
+        'height': '50px'
+      })),
+      transition( '* => *', animate(500) )
     ])
   ],
   templateUrl: './dashboard.component.html',
