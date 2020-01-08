afb599211c9a33c02a44b109a09e8bd87690a9c2
azaac <arielsantos.ac@gmail.com>
2020-01-08 00:10:42 +0000

    +using cubic-bezier to animation

--- AnimateApplication/src/app/dashboard/dashboard.component.ts
+++ AnimateApplication/src/app/dashboard/dashboard.component.ts
@@ -10,17 +10,19 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       })),
       state('go', style ({
         'background-color': 'green',
-        'width': '100px',
+        // 'width': '100px',
         'height': '100px'
       })),
       state('stop', style ({
         'background-color': 'red',
-        'width': '50px',
-        'height': '50px'
+        // 'width': '50px',
+        'height': '100px'
       })),
       // use void not to start animation when load page
       // transition( 'void => *', animate(100) ),
-      transition( '* => *', animate(500) )
+      // transition( '* => *', animate(500) )
+      transition( '* => *', animate('2s 1s cubic-bezier(0.1, 0.03, 0.685, 0.22)') )
+      // use cubic-bezier to replace ease-out, more options in https://easings.net
     ])
   ],
   templateUrl: './dashboard.component.html',
